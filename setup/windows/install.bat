43d023ef073e211281c326904e2911dc693c7022
segur <segur.opus@gmail.com>
2019-10-21 11:46:10 +0900

    Added chocolatey script

---
+++ setup/windows/install.bat
@@ -0,0 +1,88 @@
+choco install -y chocolateygui
+
+rem runtime
+choco install -y flashplayerplugin
+choco install -y flashplayeractivex
+choco install -y jre8
+choco install -y corretto8jdk
+
+rem web browser
+choco install -y googlechrome
+choco install -y firefox
+
+rem viewer
+choco install -y adobereader
+choco install -y xmind
+
+rem zip
+choco install -y 7zip.install
+
+rem chat
+choco install -y slack
+
+rem storage
+choco install -y google-drive-file-stream
+
+rem version manager
+choco install -y git
+choco install -y sourcetree
+choco install -y tortoisegit
+choco install -y hg
+
+rem virtual machine
+choco install -y docker-desktop --pre
+
+rem editor
+choco install -y typora
+choco install -y vscode
+choco install -y springtoolsuite
+choco install -y terraform
+
+rem programming language
+choco install -y nodist
+choco install -y php
+choco install -y python2
+choco install -y python
+choco install -y ruby
+choco install -y golang
+choco install -y arduino
+
+rem terminal
+choco install -y teraterm
+choco install -y winscp
+choco install -y putty
+
+rem rest client
+choco install -y curl
+choco install -y postman
+choco install -y jmeter
+
+rem database client
+choco install -y mysql.workbench
+choco install -y robo3t
+choco install -y pgadmin3
+
+rem cloud client
+choco install -y awscli
+
+rem compiler
+choco install -y cmake
+choco install -y hugo
+choco install -y hugo-extended
+
+rem creater
+choco install -y audacity
+choco install -y ffmpeg
+choco install -y imagemagick.app
+choco install -y mediainfo
+choco install -y blender
+
+rem game
+choco install -y steam
+
+rem font
+choco install -y fonts-ricty-diminished
+choco install -y myrica
+choco install -y cica
+
+pause
